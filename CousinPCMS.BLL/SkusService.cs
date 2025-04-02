3ca82c07a9471ef994b8dbb3ea309d5047d2c85e
Shweta Trivedi <st@akkomplish.com>
2025-04-02 10:23:27 +0530

    Added Skus related apis

---
+++ CousinPCMS.BLL/SkusService.cs
@@ -0,0 +1,91 @@
+﻿using Akkomplish.Framework;
+using CousinPCMS.Domain;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace CousinPCMS.BLL
+{
+    public class SkusService
+    {
+        OauthToken Oauth;
+        public SkusService(OauthToken oauthToken)
+        {
+            Oauth = oauthToken;
+        }
+
+        public APIResult<List<RelatedSkusModel>> GetRelatedSkus()
+        {
+            APIResult<List<RelatedSkusModel>> returnValue = new APIResult<List<RelatedSkusModel>>
+            {
+                IsError = false,
+                IsSuccess = true,
+            };
+            try
+            {
+                var response = ServiceClient.PerformAPICallWithToken(Method.Get, $"{HardcodedValues.PrefixBCUrl}{HardcodedValues.TenantId}{HardcodedValues.SuffixBCUrl}relatedskus?company={HardcodedValues.CompanyName}", ParameterType.GetOrPost, Oauth.Token).Content;
+
+                if (!string.IsNullOrEmpty(response))
+                {
+                    var responseOfOrderLine = JsonConvert.DeserializeObject<ODataResponse<List<RelatedSkusModel>>>(response);
+                    if (responseOfOrderLine != null && responseOfOrderLine.Value != null && responseOfOrderLine.Value.Any() && responseOfOrderLine.Value.Count > 0)
+                    {
+                        returnValue.Value = responseOfOrderLine.Value;
+                    }
+                    else
+                    {
+                        returnValue.IsSuccess = false;
+                    }
+                }
+                else
+                {
+                    returnValue.IsSuccess = false;
+                }
+            }
+            catch (Exception exception)
+            {
+                returnValue.IsSuccess = false;
+                returnValue.IsError = true;
+                returnValue.ExceptionInformation = exception;
+            }
+            return returnValue;
+        }
+
+        public APIResult<List<SkusRelationTypeModel>> GetSkusRelationType()
+        {
+            APIResult<List<SkusRelationTypeModel>> returnValue = new APIResult<List<SkusRelationTypeModel>>
+            {
+                IsError = false,
+                IsSuccess = true,
+            };
+            try
+            {
+                var response = ServiceClient.PerformAPICallWithToken(Method.Get, $"{HardcodedValues.PrefixBCUrl}{HardcodedValues.TenantId}{HardcodedValues.SuffixBCUrl}skurelationtypes?company={HardcodedValues.CompanyName}", ParameterType.GetOrPost, Oauth.Token).Content;
+
+                if (!string.IsNullOrEmpty(response))
+                {
+                    var responseOfOrderLine = JsonConvert.DeserializeObject<ODataResponse<List<SkusRelationTypeModel>>>(response);
+                    if (responseOfOrderLine != null && responseOfOrderLine.Value != null && responseOfOrderLine.Value.Any() && responseOfOrderLine.Value.Count > 0)
+                    {
+                        returnValue.Value = responseOfOrderLine.Value;
+                    }
+                    else
+                    {
+                        returnValue.IsSuccess = false;
+                    }
+                }
+                else
+                {
+                    returnValue.IsSuccess = false;
+                }
+            }
+            catch (Exception exception)
+            {
+                returnValue.IsSuccess = false;
+                returnValue.IsError = true;
+                returnValue.ExceptionInformation = exception;
+            }
+            return returnValue;
+        }
+
+    }
+}
---
+++ CousinPCMS.Domain/SkusModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace CousinPCMS.Domain
+{
+    public class SkusModel
+    {
+    }
+    public class RelatedSkusModel
+    {
+        [JsonProperty("@odata.etag")]
+        public string ODataEtag { get; set; }
+
+        [JsonProperty("itemNo")]
+        public string ItemNo { get; set; }
+
+        [JsonProperty("relatedItemNo")]
+        public string RelatedItemNo { get; set; }
+
+        [JsonProperty("relationType")]
+        public string RelationType { get; set; }
+    }
+
+    public class SkusRelationTypeModel
+    {
+        [JsonProperty("@odata.etag")]
+        public string ODataEtag { get; set; }
+
+        [JsonProperty("relationCode")]
+        public string RelationCode { get; set; }
+
+        [JsonProperty("relationDescription")]
+        public string RelationDescription { get; set; }
+    }
+}
